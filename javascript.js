7395a7c4c21516cb3e8b8af9c7ef1cdfab9a5ea0
khoo02 <khooiverson02@gmail.com>
2025-05-23 12:50:20 -0700

    finish grid function and implement color logic

--- javascript.js
+++ javascript.js
@@ -1,32 +1,85 @@
-// create container reference to store cells
+// container reference to store cells
 const container = document.querySelector("#grid-container");
 
-// create 16x16 grid in the dom
-for (let i = 0; i < 16; i++) {
-    
-    // rows
-    const row = document.createElement("div");
+// random color generator
+function getRandomColor() {
 
-        row.classList.add("grid-row");
-        // row.textContent = "row " + i;
-        container.appendChild(row);
+    let values = "0123456789ABCDEF"; // valid characters
+    let color = "#"; // color value starting with '#'
 
-    // columns
-    for (let j = 0; j < 16; j++) {
+        // calculate hex value
+        for (let i = 0; i < 6; i++) {
 
-        const col = document.createElement("div");
+            // pick character using random index and append
+            color += values[Math.floor(Math.random() * 16)];
 
-            col.classList.add("grid-col");
+        }
 
-            // name each cell
-            // col.textContent = "column [" + i + "," + j + "]";
-            row.appendChild(col); 
-
-            // set up "hover" effect
-                // add event listener to reference
-                // change color when mouse hovers
-
-    }
-
+    // returns random hex value with 6 characters
+    return color;
 }
+
+function createGrid(size) {
+
+    // change background color
+    container.style.background = "rgb(0,0,0)";
+
+    // 16x16 grid in the dom
+    for (let i = 0; i < size; i++) {
+        
+        // rows
+        // add class list and append to container
+        const row = document.createElement("div");
+        row.classList.add("grid-row");
+        container.appendChild(row);
+    
+        // columns
+        for (let j = 0; j < size; j++) {
+    
+            // add class list and append to row
+            const col = document.createElement("div");
+            col.classList.add("grid-col"); // grid cell
+            row.appendChild(col); 
+    
+            // set opacity to max
+            let opacity = 1;
+    
+            // set up "hover" effect
+            col.addEventListener("mouseover", function(e) {
+    
+                // if not colored, change then toggle flag
+                if (!col.dataset.colored) {
+    
+                    // extra credit: randomize color
+                    e.target.style.background = getRandomColor();
+                    col.dataset.colored = "true";
+    
+                }
+    
+                // reduce opacity by 10% until 0%
+                if (opacity > 0) {
+                    
+                    // reduce by 10%
+                    opacity -= 0.1;
+                    e.target.style.opacity = opacity;
+    
+                }   
+
+                }
+            )
+        }
+    }
+}
+
+// create initial 16x16 grid
+createGrid(16);
+
+// reference to controls
+controls = document.querySelector("#controls");
+
+// create button in dom and append
+const button = document.createElement("button");
+button.classList.add("button");
+controls.appendChild(button);
+
 
